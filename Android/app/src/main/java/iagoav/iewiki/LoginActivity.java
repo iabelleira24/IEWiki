983c53002188878ed3c95ac0826be6e83f40c3d1
iabelleirav24 <iabelleirav@fpcoruna.afundacion.org>
2025-04-24 09:08:30 +0200

    Actividad de launch y actividad principal (vacía)

--- Android/app/src/main/java/iagoav/iewiki/LoginActivity.java
+++ Android/app/src/main/java/iagoav/iewiki/LoginActivity.java
@@ -2,17 +2,14 @@ package iagoav.iewiki;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ Android/app/src/main/java/iagoav/iewiki/StartActivity.java
@@ -0,0 +1,31 @@
+package iagoav.iewiki;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false); // false por defecto
+
+        Intent intent;
+        if (isLoggedIn) {
+            // Si está logueado, ir a MainActivity
+            intent = new Intent(this, MainActivity.class);
+        } else {
+            // Si NO está logueado, ir a LoginActivity
+            intent = new Intent(this, LoginActivity.class);
+        }
+
+        startActivity(intent);
+        finish();
+    }
+}
