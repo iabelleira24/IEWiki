8fef2ce9673ca2c493d843516bb228f899fa2abc
iabelleirav24 <iabelleirav@fpcoruna.afundacion.org>
2025-04-24 09:15:33 +0200

    Modelos Equipo, Jugador, Videojuego, VideojuegoEquipo

--- Django/IEWiki/Api/IEWikiApi/IEWikiapp/IEWikiapp01/admin.py
+++ Django/IEWiki/Api/IEWikiApi/IEWikiapp/IEWikiapp01/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Jugador, Equipo, Videojuego, VideojuegoEquipo
+admin.site.register(Jugador)
+admin.site.register(Equipo)
+admin.site.register(Videojuego)
+admin.site.register(VideojuegoEquipo)
--- Django/IEWiki/Api/IEWikiApi/IEWikiapp/IEWikiapp01/models.py
+++ Django/IEWiki/Api/IEWikiApi/IEWikiapp/IEWikiapp01/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Equipo(models.Model):
+    nombreE = models.CharField(max_length=100)
+    entrenador = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombreE
+
+class Jugador(models.Model):
+    nombreJ = models.CharField(max_length=100)
+    posicion = models.CharField(max_length=50)
+    supertecnica = models.CharField(max_length=100)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name="jugadores")
+
+    def __str__(self):
+        return self.nombreJ
+
+class Videojuego(models.Model):
+
+    class PlataformaChoices(models.TextChoices):
+        NDS = 'NDS', 'Nintendo DS'
+        N3DS = 'N3DS', 'Nintendo 3DS'
+        WII = 'WII', 'Nintendo Wii'
+
+    nombreVideojuego = models.CharField(max_length=200)
+    jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    plataforma = models.CharField(
+        max_length=5,
+        choices=PlataformaChoices.choices,
+
+    )
+
+    def __str__(self):
+        return self.nombreVideojuego
+
+
+
+
+class VideojuegoEquipo(models.Model):
+
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    videojuego = models.ForeignKey(Videojuego, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "equipo" + str(self.equipo) + "videojuego" + str(self.videojuego)
